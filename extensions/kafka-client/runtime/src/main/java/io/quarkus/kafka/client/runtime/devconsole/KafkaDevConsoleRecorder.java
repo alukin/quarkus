bced6819b4ced52533e6f3d040c0ed5e249c8a54
Oleksiy Lukin <alukin@gmail.com>
2022-06-05 13:04:46 +0300

    POST handler works

--- extensions/kafka-client/runtime/src/main/java/io/quarkus/kafka/client/runtime/devconsole/KafkaDevConsoleRecorder.java
+++ extensions/kafka-client/runtime/src/main/java/io/quarkus/kafka/client/runtime/devconsole/KafkaDevConsoleRecorder.java
@@ -1,6 +1,11 @@
 package io.quarkus.kafka.client.runtime.devconsole;
 
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.quarkus.arc.Arc;
 import io.quarkus.devconsole.runtime.spi.DevConsolePostHandler;
+import io.quarkus.kafka.client.runtime.KafkaAdminClient;
 import io.quarkus.runtime.annotations.Recorder;
 import io.vertx.core.Handler;
 import io.vertx.core.MultiMap;
@@ -11,16 +16,41 @@ import io.vertx.ext.web.RoutingContext;
  */
 @Recorder
 public class KafkaDevConsoleRecorder {
+
     public Handler<RoutingContext> kafkaControlHandler() {
         return new DevConsolePostHandler() {
             @Override
-            protected void handlePost(io.vertx.ext.web.RoutingContext event, MultiMap form) {
-
+            protected void handlePost(RoutingContext event, MultiMap form) throws Exception {
+                String action = form.get("action");
+                String key = form.get("key");
+                String data = form.get("value");
+                System.err.println("========== POST handler called. Ation: " + action + " key: " + key + " data: " + data);
+                performAction(action, key, data);
+                endResponse(event, OK, "all good");
             }
 
             @Override
             protected void actionSuccess(RoutingContext event) {
             }
+
+            private void endResponse(RoutingContext event, HttpResponseStatus status, String message) {
+                event.response().setStatusCode(status.code());
+                event.response().end(message);
+            }
+
+            private KafkaAdminClient kafkaAdminClient() {
+                return Arc.container().instance(KafkaAdminClient.class).get();
+            }
+
+            private boolean performAction(String action, String key, String data) {
+                KafkaAdminClient adminClient = kafkaAdminClient();
+                boolean res = true;
+                if ("createTopic".equals(action)) {
+                    adminClient.createTopic(key);
+                }
+                return res;
+            }
         };
     }
+
 }
