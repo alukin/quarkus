88c6d0d4d983aaac24c9716d1281b297902befee
Oleksiy Lukin <alukin@gmail.com>
2022-07-27 12:57:45 +0300

    ConsumerFactory moved to ui

--- extensions/kafka-client/runtime/src/main/java/io/quarkus/kafka/client/runtime/ui/KafkaUiUtils.java
+++ extensions/kafka-client/runtime/src/main/java/io/quarkus/kafka/client/runtime/ui/KafkaUiUtils.java
@@ -1,6 +1,6 @@
 package io.quarkus.kafka.client.runtime.ui;
 
-import static io.quarkus.kafka.client.runtime.util.ConsumerFactory.createConsumer;
+import static io.quarkus.kafka.client.runtime.ui.util.ConsumerFactory.createConsumer;
 
 import java.util.*;
 import java.util.concurrent.ExecutionException;
